c32282d32d7d77cb863599a33131f48e9f37620f
Sam Franusic <samuel@ric.ricoh.com>
2018-06-14 18:12:38 -0700

    Extend arrays of integers with a bubbleSorted method

--- ChicosTacos.playground/Sources/BubbleSorted.swift
+++ ChicosTacos.playground/Sources/BubbleSorted.swift
@@ -2,7 +2,28 @@ import Foundation
 
 public extension Collection where Self == [Int] {
   public func bubbleSorted() -> [Int] {
-    // TODO: Add method for sorting array argument 
-    return [0]
+    guard self.count > 1 else {
+        return self
+    }
+    
+    var swapCount = 0
+    var mutableA: [Int] = self
+    for _ in 0...mutableA.count - 1 {
+        for j in 0...mutableA.count - 2 {
+            if mutableA[j] > mutableA[j + 1] {
+                mutableA.swapAt(j, j + 1)
+                swapCount += 1
+            }
+        }
+    }
+    
+    print("""
+        Array is sorted in \(swapCount) swaps.
+        First Element: \(mutableA.first!)
+        Last Element: \(mutableA.last!)
+        """)
+    return mutableA
   }
 }
+
+

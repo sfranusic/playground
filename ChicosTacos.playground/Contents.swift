0ab5f07c98f514d67005259435641543dc9851cd
Sammy Franusic <sfranusic@gmail.com>
2018-06-14 17:25:33 -0700

    Add playground

---
+++ ChicosTacos.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+let arr = [3, 2, 1]
+print(arr.bubbleSorted())
+
+
---
+++ ChicosTacos.playground/Sources/BubbleSorted.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public extension Collection where Self == [Int] {
+  public func bubbleSorted() -> [Int] {
+    // TODO: Add method for sorting array argument 
+    return [0]
+  }
+}

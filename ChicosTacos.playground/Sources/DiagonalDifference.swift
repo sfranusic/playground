e14d8f9ab07508bc8d51b29c4d532d15d9c9c93a
Sam Franusic <samuel@ric.ricoh.com>
2018-06-19 15:47:26 -0700

    Refactor diagonal difference solution

--- ChicosTacos.playground/Sources/DiagonalDifference.swift
+++ ChicosTacos.playground/Sources/DiagonalDifference.swift
@@ -1,35 +1,13 @@
 import Foundation
 
-public func diagonalDifference(a: [[Int]]) -> Int {
+public func diagonalDifference(arr: [[Int]]) -> Int {
+
+  let n = arr.count - 1
+  var difference = 0
   
-  guard a.count > 0 else {
-    return 0
-  }
-  
-  // Guard that matrix has even width and height
-  guard a.count == a[0].count else {
-    return 0
-  }
-  
-  let n = a.count - 1
-  
-  //Top left to bottom right diagonal
-  var diagonal1 = 0
   for i in 0...n {
-    diagonal1 += a[i][i]
+    difference += (arr[i][i] - arr[i][n - i])
   }
   
-  //Bottom left to top right diagonal
-  var diagonal2 = 0
-  for i in 0...n {
-    diagonal2 += a[n - i][i]
-  }
-  
-  
-  print("""
-    sum of diagonal1 = \(diagonal1)
-    sum of diagonal2 = \(diagonal2)
-    aboslute sum of both = \(abs(diagonal1) >= abs(diagonal2) ? abs(diagonal1) - abs(diagonal2) : abs(diagonal2) - abs(diagonal1))
-""")
-  return abs(diagonal1) >= abs(diagonal2) ? abs(diagonal1) - abs(diagonal2) : abs(diagonal2) - abs(diagonal1)
+  return abs(difference)
 }

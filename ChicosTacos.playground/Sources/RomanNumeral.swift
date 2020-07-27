0999099d4c5e1461c9ee354b5ccb8c10a1170434
Samuel Franusic <sfranusic@gmail.com>
2020-07-27 12:56:27 -0700

    Implement roman numeral generator

---
+++ ChicosTacos.playground/Sources/RomanNumeral.swift
@@ -0,0 +1,67 @@
+import Foundation
+
+public func romanNumeral(_ number: Int) -> String? {
+
+    guard number < 10000 else {
+        print("Roman numerals above 10000 not supported currently.")
+        return nil
+    }
+
+    switch number {
+
+    case 1000...10000:
+        let remainder = number % 1000
+        return "\(String(Array(repeating: "M", count: number / 1000)))\(romanNumeral(remainder) ?? "")"
+
+    case 900...999:
+        // Special case.
+        return "CM\(romanNumeral(number - 900) ?? "")"
+
+    case 500...899:
+        let remainder = number % 500
+        return "\(String(Array(repeating: "D", count: number / 500)))\(romanNumeral(remainder) ?? "")"
+
+    case 400...499:
+        // Special case.
+        return "CD\(romanNumeral(number - 400) ?? "")"
+
+    case 100...399:
+        let remainder = number % 100
+        return "\(String(Array(repeating: "C", count: number / 100)))\(romanNumeral(remainder) ?? "")"
+
+    case 90...99:
+        // Special case.
+        return "XC\(romanNumeral(number - 90) ?? "")"
+
+    case 50...89:
+        let remainder = number % 50
+        return "\(String(Array(repeating: "L", count: number / 50)))\(romanNumeral(remainder) ?? "")"
+
+    case 40...49:
+        // Special case.
+        return "XL\(romanNumeral(number - 40) ?? "")"
+
+    case 10...39:
+        let remainder = number % 10
+        return "\(String(Array(repeating: "X", count: number / 10)))\(romanNumeral(remainder) ?? "")"
+
+    case 9:
+        // Special case.
+        return "IX"
+
+    case 5...8:
+        let remainder = number % 5
+        return "\(String(Array(repeating: "V", count: number / 5)))\(romanNumeral(remainder) ?? "")"
+
+    case 4:
+        // Special case.
+        return "IV"
+
+    case 0...3:
+        let remainder = number % 1
+        return "\(String(Array(repeating: "I", count: number / 1)))\(romanNumeral(remainder) ?? "")"
+
+    default:
+        return nil
+    }
+}

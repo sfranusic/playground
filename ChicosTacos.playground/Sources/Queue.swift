4806af07b46b31d09ed357c2df2fb4706fc186d2
Samuel Franusic <sfranusic@gmail.com>
2020-08-07 10:43:11 -0700

    Add property to check type

--- ChicosTacos.playground/Sources/Queue.swift
+++ ChicosTacos.playground/Sources/Queue.swift
@@ -4,6 +4,10 @@ public struct Queue<Element> {
 
     private(set) var elements = [Element]()
 
+    var isReferenceType: Bool {
+        type(of: Element.self) is AnyClass
+    }
+
     public init() { }
 
     public mutating func add(_ element: Element) {
